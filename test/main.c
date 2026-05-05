5c20d46fd50c2d9da65eeabfb26287471d4ce65f
wiirios <recuperacaowilliamrios@gmail.com>
2026-05-05 16:05:53 -0300

    test: improved code readability for the creation of the trash bin

--- test/main.c
+++ test/main.c
@@ -10,32 +10,29 @@
 
 #ifdef DEBUG
 int main(int argc, char* argv[]) {
-    // move_file_match_pattern(argv[1], argv[2]);
-
     if (argc < 2) error("Insufficient arguments");
     
-    char BUFFER_PATH[MAX_BIN_PATH_NAME];
-    get_bin_path(BUFFER_PATH, sizeof(BUFFER_PATH));
-
-    size_t buffer_l = strlen(BUFFER_PATH);
-
-    create_bin(BUFFER_PATH);
-    DIR *dir = opendir(BUFFER_PATH);
+    char BIN_PATH[MAX_BIN_PATH_NAME], LIST_BIN_PATH[MAX_BIN_PATH_NAME];
+    get_bin_path(BIN_PATH, sizeof(BIN_PATH));
+    get_bin_path(BIN_PATH, sizeof(LIST_BIN_PATH));
+    
+    create_bin(BIN_PATH);
+    DIR *dir = opendir(BIN_PATH);
+    
     if (!check_if_file_exist(dir, "list.txt")) {
-        strcat(BUFFER_PATH, "/list.txt");
-        FILE *f_ = fopen(BUFFER_PATH, "w");
+        strcat(LIST_BIN_PATH, "/list.txt");
+        FILE *f_ = fopen(LIST_BIN_PATH, "w");
         
         if (!f_) error("Cannot create list.txt");
         
         fclose(f_);
     }
     closedir(dir);
+    
+    char USER_PATH[MAX_FILE_NAME];
+    GetCurrentDir(USER_PATH, FILENAME_MAX);
 
-    BUFFER_PATH[buffer_l] = '\0';
-    strcat(BUFFER_PATH, "/trash");
-    create_bin(BUFFER_PATH);
-
-    if (strcmp(argv[1], "move") == 0 && argv[2] != NULL) move_debug(argv[2], 0);
+    if (strcmp(argv[1], "move") == 0 && argv[2] != NULL) move_debug(BIN_PATH, USER_PATH, argv[2], 0, 1);
     else if (strcmp(argv[1], "list") == 0) list_debug();
     else if (strcmp(argv[1], "match-move") == 0) move_file_match_pattern(argv[1]);
     else if (strcmp(argv[1], "clean") == 0) clean();
