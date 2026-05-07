c2e0acbf958475d9b22cc53e66216a12c653d4d0
wiirios <recuperacaowilliamrios@gmail.com>
2026-05-07 14:33:05 -0300

    feat: create documentation for all core functions

--- src/headers/core.h
+++ src/headers/core.h
@@ -9,8 +9,26 @@
 #include <sys/types.h>
 #endif
 
+/**
+ * @brief Print an error message and terminate the program.
+ *
+ * @param msg Null-terminated error message string.
+ */
 void error(const char *msg);
+
+/**
+ * @brief Fill a buffer with the application origin path.
+ *
+ * @param buffer Destination buffer to receive the origin path.
+ * @param size Size of the destination buffer.
+ */
 void get_origin_path(char *buffer, size_t size);
+
+/**
+ * @brief Create the trash bin directory if it does not exist.
+ *
+ * @param bin_path Path to the bin directory to create.
+ */
 void create_bin(const char *bin_path);
 
 #endif
